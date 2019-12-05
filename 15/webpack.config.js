c7247dc54df64b2aabfa2daa50dc6fbb76d6f1be
tsl <taosiliang1127tsl@163.com>
2019-12-05 16:04:23 +0800

    解析es6和jsx

---
+++ 15/webpack.config.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+    entry:{
+        index: './src/index.js',
+        search:'./src/search.js'
+    },
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: '[name].js'
+    },
+    mode:'production',
+    module:{
+        rules:[
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
