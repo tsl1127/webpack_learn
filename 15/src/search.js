25914a499775f3c7bafb4b1cc00453a3ab54ccd7
tsl <taosiliang1127tsl@163.com>
2019-12-05 16:20:15 +0800

    解析css

--- 15/src/search.js
+++ 15/src/search.js
@@ -4,12 +4,13 @@
 "use strict"
 import React from "react"
 import ReactDOM from "react-dom"
+import './search.css'
 
 
 class Search extends React.Component {
     render () {
         return (
-            <div>search text</div>
+            <div className="search-text">search text</div>
         )
     }
 }
