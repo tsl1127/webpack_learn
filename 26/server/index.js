754eb24c8f4bea4910489b9b0f68066116df18b7
tsl <taosiliang1127tsl@163.com>
2020-01-07 23:01:22 +0800

    通过占位符的方式解决css服务端不显示的问题

--- 26/server/index.js
+++ 26/server/index.js
@@ -2,11 +2,12 @@ if (typeof window === "undefined") {
     global.window = {}
 }
 
-
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const { renderToString } = require('react-dom/server')
 const SSR = require('../dist/search-server')
-
+const template = fs.readFileSync(path.join(__dirname, '../dist/search.html'), 'utf-8')
 
 const server = (port) => {
     const app = express()
@@ -25,22 +26,7 @@ server(process.env.PORT || 3000)
 
 
 const renderMarkup = (str) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-    <div id="root">
-        ${str}
-    </div>
-    </body>
-    </html>
-    `
+    return template.replace('<!--HTML_ZW-->', str)  //占位符替换
 }
 
 //启server服务  node server/index.js
