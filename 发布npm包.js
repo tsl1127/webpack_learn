5a39ec33b328c46a6c7e4cb078608a1dfb5135a4
tsl <taosiliang1127tsl@163.com>
2020-01-12 16:53:24 +0800

    发布npm包

---
+++ 发布npm包.js
@@ -0,0 +1,11 @@
+发布到npm
+添加用户：npm adduser 
+升级版本：
+    升级补丁版本号：npm version patch   会自动提交到仓库，也会自动帮你打tag
+    升级小版本号：npm version minor 
+    升级大版本号：npm version major
+
+发布版本：npm publish 
+
+
+每次记得打tag   npm tag v1.0.1
