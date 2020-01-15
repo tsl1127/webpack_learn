dc29f53b872b726c0303904450783818e988efd2
tsl <taosiliang1127tsl@163.com>
2020-01-15 18:11:54 +0800

    动态polyfill

---
+++ 动态polyfill.js
@@ -0,0 +1,16 @@
+
+方案：
+babel-polyfill 
+babel-plugin-transform-runtime 
+自己写Map、Set的polyfill
+polyfill-service 
+
+
+polyfill service原理
+识别User Agent（不同的设备，比如iphone5 iphoneX等），下发不同的Polyfill
+
+构建体积优化：如何使用动态Polyfill service 
+polyfill.io 官方提供的服务
+
+基于官方自建polyfill服务
+
